a9487fcdcc5e37610d248d46059ded0d9e4edb4a
Kamal Salisu <dev@kamalsalisu.com>
2026-08-19 01:45:37 +0000

    added ingest script

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,117 +1,71 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
 
-
+import click
 import pandas as pd
-
-
-# In[10]:
-
-
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
-
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-df = pd.read_csv(
-    prefix + 'yellow_tripdata_2021-01.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates
-)
-
-
-# In[11]:
-
-
-df.head()
-
-
-# In[12]:
-
-
-df.dtypes
-
-
-# In[8]:
-
-
-df.shape
-
-
-# In[13]:
-
-
-get_ipython().system('uv add sqlalchemy "psycopg[binary,pool]"')
-
-
-# In[14]:
-
-
 from sqlalchemy import create_engine
-engine = create_engine('postgresql+psycopg://root:root@localhost:5432/ny_taxi')
-
-
-# In[15]:
-
-
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[16]:
-
-
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[21]:
-
-
 from tqdm.auto import tqdm
 
 
-# In[22]:
+@click.command()
+@click.option("--pg-user", default="root", help="PostgreSQL user")
+@click.option("--pg-pass", default="root", help="PostgreSQL password")
+@click.option("--pg-host", default="localhost", help="PostgreSQL host")
+@click.option("--pg-port", default=5432, type=int, help="PostgreSQL port")
+@click.option("--pg-db", default="ny_taxi", help="PostgreSQL database name")
+@click.option("--target-table", default="yellow_taxi_data", help="Target table name")
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
 
+    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
+    dtype = {
+        "VendorID": "Int64",
+        "passenger_count": "Int64",
+        "trip_distance": "float64",
+        "RatecodeID": "Int64",
+        "store_and_fwd_flag": "string",
+        "PULocationID": "Int64",
+        "DOLocationID": "Int64",
+        "payment_type": "Int64",
+        "fare_amount": "float64",
+        "extra": "float64",
+        "mta_tax": "float64",
+        "tip_amount": "float64",
+        "tolls_amount": "float64",
+        "improvement_surcharge": "float64",
+        "total_amount": "float64",
+        "congestion_surcharge": "float64",
+    }
 
-df_iter = pd.read_csv(
-    prefix + 'yellow_tripdata_2021-01.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=100000
-)
+    parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
+    engine = create_engine(
+        f"postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+    )
 
-# In[23]:
+    df_iter = pd.read_csv(
+        prefix + "yellow_tripdata_2021-01.csv.gz",
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=100000,
+    )
 
+    first = True
 
-for df_chunk in tqdm(df_iter):
-    df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+    for df_chunk in tqdm(df_iter):
+        if first:
+            # Create table schema (no data)
+            df_chunk.head(0).to_sql(
+                name=target_table, con=engine, if_exists="replace"
+            )
+            first = False
+            print("Table created")
 
+        # Insert chunk
+        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
 
-# In[ ]:
+        print("Inserted:", len(df_chunk))
 
-
-
-
+if __name__ == '__main__':
+    run()
